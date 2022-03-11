4911bd901afdafb9d20bc28164032b52b0003d03
TChoy04 <tszfai04@gmail.com>
2022-03-10 21:33:05 -0500

    Edit anchor tag - Resume

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -13,7 +13,12 @@ const AboutMe = () => {
           If you would like to get in contact with me, my social links are right
           above and you can expect a response within 24 hours.
         </p>
-        <a className="resume hover-link" href="./docs/Tszfai_Choy_Resume">
+        <a
+          className="resume hover-link"
+          href="./docs/Tszfai_Choy_Resume"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Resume Link
         </a>
       </div>
