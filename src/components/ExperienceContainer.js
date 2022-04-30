131f27f0944b3a851809e78152f266cbe459a5da
TChoy04 <tszfai04@gmail.com>
2022-04-30 00:48:50 -0400

    Update ExperienceContainer.js

--- src/components/ExperienceContainer.js
+++ src/components/ExperienceContainer.js
@@ -10,29 +10,31 @@ const ExperienceContainer = () => {
           Organizations I've worked with.
         </strong>
       </div>
-      <div className="split-exp">
-        <Experiences
-          title="International Socioeconomics Laboratory"
-          role="Board of Directors - Web Development"
-          date="March 2020 - Feb 2022"
-        />
-        <Experiences
-          title="Finxerunt Policy Institute"
-          role="Web Developer"
-          date="March 2020 - Feb 2022"
-        />
-      </div>
-      <div className="split-exp">
-        <Experiences
-          title="STEM from Scratch"
-          role="Writer + Editor"
-          date="March 2021 - Present"
-        />
-        <Experiences
-          title="Brooklyn Public Library"
-          role="Technical Support Internship"
-          date="September 2019 - May 2020"
-        />
+      <div>
+        <div className="split-exp">
+          <Experiences
+            title="International Socioeconomics Laboratory"
+            role="Board of Directors - Web Development"
+            date="March 2020 - Feb 2022"
+          />
+          <Experiences
+            title="Finxerunt Policy Institute"
+            role="Web Developer"
+            date="March 2020 - Feb 2022"
+          />
+        </div>
+        <div className="split-exp">
+          <Experiences
+            title="STEM from Scratch"
+            role="Writer + Editor"
+            date="March 2021 - Present"
+          />
+          <Experiences
+            title="Brooklyn Public Library"
+            role="Technical Support Internship"
+            date="September 2019 - May 2020"
+          />
+        </div>
       </div>
     </div>
   );
