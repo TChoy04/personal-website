5d3968379d127a47d7732fb16027475b9d82824d
Tszfai <tszfai04@gmail.com>
2022-09-15 20:37:22 -0400

    attempting to fix redirects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as  Router, Routes, Route} from "react-router-dom";
 import Main from "./components/main.js";  
 import "./App.css";
 import Planner from  "./planner/Planner"
+import ErrorPage from "./components/ErrorPage.js"
 function App() {
   return (
 <Router>
@@ -9,6 +10,7 @@ function App() {
     <Route path="/" element={<Main></Main>}></Route>
     <Route path="/js30"> </Route>
     <Route path ="planner" element={<Planner></Planner>}></Route>
+    <Route path="*" element={<ErrorPage></ErrorPage>}></Route>
   </Routes>
 </Router>
   );
