aab964a0a028659cf33e969074b2ccb13651def1
Tszfai <tszfai04@gmail.com>
2022-09-20 22:01:43 -0400

    added image

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -2,16 +2,17 @@ import "./MyHeader.css";
 import SocialIcons from "./socials.js";
 import AboutMe from "./AboutMe.js";
 import "../App.css"
+import Tszfai from "./images/Tszfai_Choy_Headshot.jpg";
 const MyHeader = () => {
   return (
     <header>
       <div className="image-socials">
         <img
           id="displayedImage"
-          src="https://dummyimage.com/300.png/09f/fff"
+          src={Tszfai}
           alt="Tszfai"
         ></img>
-
+ 
         <div className="myInfo">
           <h1>Tszfai Choy</h1>
           <h3>Software Engineer</h3>
