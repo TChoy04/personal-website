46047a748735283eb56563ce36d0e8fb0a13d5c7
Tszfai <tszfai04@gmail.com>
2022-09-27 18:49:06 -0400

    ran into bug trying to fix

--- src/love_worm/Board.js
+++ src/love_worm/Board.js
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+import { render } from '@testing-library/react';
+import React,{useEffect, useState} from 'react'
 import "./Board.css"
 import Heart from "./Heart";
 import Worm from "./WormBody.js"
@@ -11,12 +12,59 @@ function randomHeartGenerator(){
     let y = randomIntFromInterval(0,19);
     return [x,y]
 }
+
+
+
 function Board() {
-    const [heart,updateHeart] = useState(randomHeartGenerator())
+    const [direction,updateDirection] = useState("RIGHT");
+    const [heart,updateHeart] = useState(randomHeartGenerator());
     const [wormBody, updateWormBody] = useState([
-        [0,0],
-        [0,1],
-    ])
+        [9,9]
+    ]);
+    const onKeyDown = (e) =>{
+        if(e.key=='ArrowLeft' || e.key == "d" || e.key == "D"){
+            updateDirection("LEFT");
+            moveSnake();
+        }
+        else if(e.key=='ArrowRight' || e.key == "a" || e.key == "A"){
+            updateDirection("RIGHT");
+            moveSnake();
+        }
+        else if(e.key=='ArrowUp' || e.key == "w" || e.key == "W"){
+            updateDirection("UP");
+            moveSnake();
+        }
+        else if(e.key=='ArrowDown' || e.key == "s" || e.key == "S"){
+            updateDirection("DOWN");
+            moveSnake();
+        }
+        return "";
+    }
+
+    const moveSnake = () =>{
+        let body = [...wormBody];
+        let head = body[body.length-1];
+        if(direction=="RIGHT"){
+            head = [head[0]+1,head[1]];
+        }
+        else if(direction=="LEFT"){
+            head = [head[0]-1,head[1]];
+        }
+        else if(direction=="DOWN"){
+            head = [head[0],head[1]+1];
+        }
+        else if(direction=="UP"){
+            head = [head[0],head[1]-1];
+        }
+        body.push(head);
+        body.shift();
+        updateWormBody(body);
+    }
+    useEffect(()=>{
+        setInterval(moveSnake,500)
+        window.addEventListener("keydown",onKeyDown);
+    });
+    render();
   return (
     <div className="board">
         <Worm wormBody={wormBody}></Worm>
--- src/love_worm/WormBody.js
+++ src/love_worm/WormBody.js
@@ -1,16 +1,27 @@
 import React from 'react'
 //Size of squares.
 const multi = 5;
+//Copied from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
 function WormBody(props) {
   return (
     <div>
         {props.wormBody.map((body,i)=>{
+              let color = getRandomColor()
             const style = {
                 left:`${body[0]*multi}%`,
                 top:`${body[1]*multi}%`,
+                background: `${color}`
             }
             return(<div className="worm-body" style={style} key ={i}
-            
             ></div>)
         })}
     </div>
